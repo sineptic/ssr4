0dcf8eda9c44b5f095399f70ac2dce938d3826a7
sineptic <sineptic0@gmail.com>
2026-04-22 22:40:57 +0300

    add task visualisation and suppress keys being directly propogated to the screen

--- src/main.zig
+++ src/main.zig
@@ -12,12 +12,22 @@ pub fn main(init: std.process.Init) !void {
     var stderr_buffer: [1024]u8 = undefined;
     var stderr_writer = std.Io.File.stderr().writer(io, &stderr_buffer);
     const stderr = &stderr_writer.interface;
+    const stdin_file = std.Io.File.stdin();
     var stdin_buffer: [1024]u8 = undefined;
-    var stdin_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
+    var stdin_reader = stdin_file.reader(io, &stdin_buffer);
     const input_buffer = try gpa.alloc(u8, 10_000);
     defer gpa.free(input_buffer);
     const input_n = try stdin_reader.interface.readSliceShort(input_buffer);
     const input = input_buffer[0..input_n];
+
+    const tty = try std.Io.Dir.cwd().openFile(io, "/dev/tty", .{});
+    defer tty.close(io);
+
+    const original_termios = try std.posix.tcgetattr(tty.handle);
+    defer std.posix.tcsetattr(tty.handle, .FLUSH, original_termios) catch {};
+    var termios = original_termios;
+    termios.lflag.ECHO = false;
+    try std.posix.tcsetattr(tty.handle, .NOW, termios);
 
     const blocks = try parse_task(gpa, stderr, input);
     defer gpa.free(blocks);
@@ -31,20 +41,33 @@ pub fn main(init: std.process.Init) !void {
         stdout.flush() catch {};
     }
 
-    try stdout.writeAll("\x1b[1;1H");
-    try stdout.print("{s}", .{input});
-    try stdout.flush();
-    try io.sleep(.fromSeconds(1), .real);
+    var cursor: usize = 0;
+    for (blocks, 0..) |block, i| {
+        if (block.is_interactive()) {
+            cursor = i;
+            break;
+        }
+    }
+    try display_blocks_interactive(stdout, blocks, cursor);
+
+    try io.sleep(.fromSeconds(5), .real);
 }
 
-const Block = struct {
-    kind: BlockKind,
-    string: []const u8,
-};
-const BlockKind = enum {
-    text,
-    hidden_text,
-    note,
+const Block = union(enum) {
+    text: []const u8,
+    hidden: struct {
+        original_text: []const u8,
+        user_input: []const u8,
+        field_cursor: usize,
+    },
+    note: []const u8,
+
+    fn is_interactive(self: Block) bool {
+        return switch (self) {
+            .text, .note => false,
+            .hidden => true,
+        };
+    }
 };
 
 fn parse_task(allocator: std.mem.Allocator, stderr: *std.Io.Writer, text_raw: []const u8) ![]Block {
@@ -60,15 +83,54 @@ fn parse_task(allocator: std.mem.Allocator, stderr: *std.Io.Writer, text_raw: []
                 stderr.flush() catch {};
                 return error.UnclosedBacktickQuote;
             };
-            try blocks.append(allocator, .{ .kind = .hidden_text, .string = block });
+            try blocks.append(allocator, .{ .hidden = .{
+                .original_text = block,
+                .user_input = &.{},
+                .field_cursor = 0,
+            } });
         } else if (next_slashes == 0) {
             const block, tail = std.mem.cut(u8, tail[2..], "\n") orelse .{ tail, &.{} };
-            try blocks.append(allocator, .{ .kind = .note, .string = block });
+            try blocks.append(allocator, .{ .note = block });
         } else {
             const plain_text_end = std.mem.min(usize, &.{ next_quote orelse tail.len, next_slashes orelse tail.len });
-            try blocks.append(allocator, .{ .kind = .text, .string = tail[0..plain_text_end] });
+            try blocks.append(allocator, .{ .text = tail[0..plain_text_end] });
             tail = tail[plain_text_end..];
         }
     }
+    if (blocks.items.len == 0) {
+        stderr.print("ERROR: You should write task.\n", .{}) catch {};
+        stderr.flush() catch {};
+        return error.EmptyTask;
+    }
     return blocks.toOwnedSlice(allocator);
+}
+
+fn display_blocks_interactive(output: *std.Io.Writer, blocks: []const Block, cursor: usize) !void {
+    try output.writeAll("\x1b[2J\x1b[1;1H\x1b[0m");
+
+    for (blocks, 0..) |block, i| {
+        switch (block) {
+            .text => |string| {
+                try output.print("{s}", .{string});
+            },
+            .note => {},
+            .hidden => |hidden_block| {
+                if (hidden_block.user_input.len == 0 and i != cursor) {
+                    try output.print("\x1b[3m<empty>\x1b[0m", .{});
+                } else {
+                    try output.print("\x1b[3;4m{s}\x1b7{s}\x1b[0m", .{
+                        hidden_block.user_input[0..hidden_block.field_cursor],
+                        hidden_block.user_input[hidden_block.field_cursor..],
+                    });
+                }
+            },
+        }
+    }
+    if (blocks[cursor].is_interactive()) {
+        try output.writeAll("\x1b8\x1b[?25h");
+    } else {
+        try output.writeAll("\x1b[?25l");
+    }
+
+    try output.flush();
 }
