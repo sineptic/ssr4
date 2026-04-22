cc375f5fc2a5dba8d7392cb817a455fca710852a
sineptic <sineptic0@gmail.com>
2026-04-22 16:35:20 +0300

    task parsing

--- src/main.zig
+++ src/main.zig
@@ -6,45 +6,69 @@ pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
     const io = init.io;
 
-    const stdout = std.Io.File.stdout();
     var stdout_buffer: [1024]u8 = undefined;
-    var output_writer = stdout.writer(io, &stdout_buffer);
-    var output = &output_writer.interface;
-    const stdin = std.Io.File.stdin();
+    var stdout_writer = std.Io.File.stdout().writer(io, &stdout_buffer);
+    var stdout = &stdout_writer.interface;
+    var stderr_buffer: [1024]u8 = undefined;
+    var stderr_writer = std.Io.File.stderr().writer(io, &stderr_buffer);
+    const stderr = &stderr_writer.interface;
     var stdin_buffer: [1024]u8 = undefined;
-    var stdin_reader = stdin.reader(io, &stdin_buffer);
+    var stdin_reader = std.Io.File.stdin().reader(io, &stdin_buffer);
     const input_buffer = try gpa.alloc(u8, 10_000);
     defer gpa.free(input_buffer);
     const input_n = try stdin_reader.interface.readSliceShort(input_buffer);
     const input = input_buffer[0..input_n];
 
+    const blocks = try parse_task(gpa, stderr, input);
+    defer gpa.free(blocks);
+
     // enter alt screen
-    try output.writeAll("\x1b[?1049h");
-    try output.flush();
+    try stdout.writeAll("\x1b[?1049h");
+    try stdout.flush();
     defer {
         // exit alt screen
-        output.writeAll("\x1b[?1049l") catch {};
-        output.flush() catch {};
+        stdout.writeAll("\x1b[?1049l") catch {};
+        stdout.flush() catch {};
     }
 
-    try output.writeAll("\x1b[1;1H");
-    try output.print("{s}", .{input});
-    try output.flush();
+    try stdout.writeAll("\x1b[1;1H");
+    try stdout.print("{s}", .{input});
+    try stdout.flush();
     try io.sleep(.fromSeconds(1), .real);
 }
 
-const Block = union(enum) {
-    text: []const u8,
-    hidden_text: []const u8,
-    note: []const u8,
+const Block = struct {
+    kind: BlockKind,
+    string: []const u8,
+};
+const BlockKind = enum {
+    text,
+    hidden_text,
+    note,
 };
 
-fn is_next(string: []const u8, pattern: []const u8) bool {
-    if (pattern.len > string.len) return false;
-    return std.mem.eql(u8, string[0..pattern.len], pattern);
-}
-fn parse_task(allocator: std.mem.Allocator, text: []const u8) ![]Block {
-    _ = text;
+fn parse_task(allocator: std.mem.Allocator, stderr: *std.Io.Writer, text_raw: []const u8) ![]Block {
+    var tail = std.mem.trim(u8, text_raw, " \n\t");
     var blocks = std.ArrayList(Block).empty;
+    errdefer blocks.deinit(allocator);
+    while (tail.len > 0) {
+        const next_quote = std.mem.find(u8, tail, "`");
+        const next_slashes = std.mem.find(u8, tail, "//");
+        if (next_quote == 0) {
+            const block, tail = std.mem.cut(u8, tail[1..], "`") orelse {
+                stderr.print("ERROR: You should close backtick quote to indicate hidden_text block end.\n", .{}) catch {};
+                stderr.flush() catch {};
+                return error.UnclosedBacktickQuote;
+            };
+            try blocks.append(allocator, .{ .kind = .hidden_text, .string = block });
+        } else if (next_slashes == 0) {
+            const block, tail = std.mem.cut(u8, tail[2..], "\n") orelse .{ tail, &.{} };
+            try blocks.append(allocator, .{ .kind = .note, .string = block });
+        } else {
+            const plain_text_end = std.mem.min(usize, &.{ next_quote orelse tail.len, next_slashes orelse tail.len });
+            try blocks.append(allocator, .{ .kind = .text, .string = tail[0..plain_text_end] });
+            tail = tail[plain_text_end..];
+        }
+    }
     return blocks.toOwnedSlice(allocator);
 }
