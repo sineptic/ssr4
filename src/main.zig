ec9b91764f67e9219ca168abc2d28d66ac35ea64
sineptic <sineptic0@gmail.com>
2026-04-22 07:52:54 +0300

    update to zig 0.16

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,17 @@ const std = @import("std");
 
 const ssr4 = @import("ssr4");
 
-pub fn main() !void {
-    var allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = allocator.allocator();
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    const io = init.io;
 
-    const stdout = std.fs.File.stdout();
+    const stdout = std.Io.File.stdout();
     var stdout_buffer: [1024]u8 = undefined;
-    var output_writer = stdout.writer(&stdout_buffer);
+    var output_writer = stdout.writer(io, &stdout_buffer);
     var output = &output_writer.interface;
-    const stdin = std.fs.File.stdin();
+    const stdin = std.Io.File.stdin();
     var stdin_buffer: [1024]u8 = undefined;
-    var stdin_reader = stdin.reader(&stdin_buffer);
+    var stdin_reader = stdin.reader(io, &stdin_buffer);
     const input_buffer = try gpa.alloc(u8, 10_000);
     defer gpa.free(input_buffer);
     const input_n = try stdin_reader.interface.readSliceShort(input_buffer);
@@ -30,7 +30,7 @@ pub fn main() !void {
     try output.writeAll("\x1b[1;1H");
     try output.print("{s}", .{input});
     try output.flush();
-    std.Thread.sleep(1_000_000_000);
+    try io.sleep(.fromSeconds(1), .real);
 }
 
 const Block = union(enum) {
@@ -44,6 +44,7 @@ fn is_next(string: []const u8, pattern: []const u8) bool {
     return std.mem.eql(u8, string[0..pattern.len], pattern);
 }
 fn parse_task(allocator: std.mem.Allocator, text: []const u8) ![]Block {
+    _ = text;
     var blocks = std.ArrayList(Block).empty;
     return blocks.toOwnedSlice(allocator);
 }
